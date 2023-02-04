69e26e4ad45afbe4649c017564501ae8981e019d
Susana_xz <riberasusana@gmail.com>
2023-02-04 13:43:26 +0100

    añado fondo del campo

--- pong.py
+++ pong.py
@@ -1,14 +1,19 @@
 import pygame #inicializar la librería
 
-ANCHO = 640
-ALTO = 480
+ANCHO = 800
+ALTO = 600
 COLOR = (245,184,65)
+COLOR_PANTALLA = (86,110,61,25)
 
 ANCHO_PALETA = 10
 ALTO_PALETA = 40
 MARGEN_LATERAL = 40
 
-TAM_PELOTA = 5
+TAM_PELOTA = 8
+TAM_LINEA = 5
+
+
+
 
 class Jugador(pygame.Rect):
     def __init__(self, pos_x, pos_y):
@@ -26,10 +31,17 @@ class Pelota(pygame.Rect):
         pygame.draw.rect(pantalla, COLOR, self.rectangulo)
 
 class Pong:
-
+ 
     def __init__(self):
         pygame.init()
         self.pantalla = pygame.display.set_mode((ANCHO, ALTO))  #crear la pantalla para el juego
+        self.pantalla.fill(COLOR_PANTALLA)
+        
+        #importar una imagen de fondo
+        fondo = pygame.image.load("img/campo.png").convert_alpha() #importar
+        self.pantalla.blit(fondo, (-1,0)) #posición de la imagen en pantalla
+         #visualizar
+        
         pos_y = (ALTO-ALTO_PALETA)/2
         pos_x_2 = ANCHO-MARGEN_LATERAL-ANCHO_PALETA
         
@@ -37,15 +49,22 @@ class Pong:
         self.jugador2 = Jugador(pos_x_2, pos_y)
         pelota_x = (ANCHO - TAM_PELOTA)/2
         pelota_y = (ALTO - TAM_PELOTA)/2
+        linea_x = (ANCHO - TAM_LINEA)/2
         self.pelota = Pelota(pelota_x, pelota_y)
+        # self.linea = pygame.draw.lines(self.pantalla, COLOR, True, [(linea_x,0),(linea_x,ALTO)], TAM_LINEA)
+        
 
-    def bucle_principal(self): #creación del bucle principal para que el juego permanezca abierto para ir actualizandose.
+    def bucle_principal(self): #creación del bucle principal para que el juego permanezca abierto para ir actualizándose.
         salir = False
         while not salir: 
             #creamos un evento para salir del bucle cuando le demos a salir en el display
             for evento in pygame.event.get():
                 if evento.type == pygame.QUIT:
                     salir = True
+                    # salir con la tecla escape
+                if evento.type == pygame.KEYUP:
+                    if evento.tecla == pygame.K_ESCAPE:
+                        salir = True
 
             #dibujo un rectangulo (x, y, ancho, alto)
             self.jugador1.pintame(self.pantalla)
@@ -54,7 +73,7 @@ class Pong:
             
             # pygame.draw.rect(self.pantalla, (COLOR), pygame.Rect(MARGEN_LATERAL, (ALTO-ALTO_PALETA)/2, ANCHO_PALETA, ALTO_PALETA))
             # pygame.draw.rect(self.pantalla, (COLOR), pygame.Rect(ANCHO-MARGEN_LATERAL-ANCHO_PALETA, (ALTO-ALTO_PALETA)/2, ANCHO_PALETA, ALTO_PALETA))
-            pygame.display.flip()  #borra la pantalla y el bucle la vuelve a pintar
+            pygame.display.flip() #borra la pantalla y el bucle la vuelve a pintar.
 
 if __name__ == "__main__":
     juego = Pong()  #inicia el constructor
